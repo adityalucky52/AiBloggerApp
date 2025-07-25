fe4d22bd63aac33ec6d37163d9fb92501bb6a440
aditya lucky <adityalucky52@gmail.com>
2025-07-25 11:20:03 +0530

    code updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,51 +1,14 @@
-// import mongoose from 'mongoose';
-// // import mongoose from "mongoose";
 
-
-
-// const connectDB = async () => {
-//   try {
-//       await mongoose.connect(`${process.env.MONGODB_URL}/quickblog`);
-//     mongoose.connection.on("connected", () => {
-//       console.log("MongoDB connection established successfully.");
-//     });
-
-    
-//   } catch (error) {
-//     console.error(`Error: ${error.message}`);
-  
-//   }
-// }
-
-
-
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MANGODB_URL, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log(`MongoDB connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error('MongoDB connection FAILED:', error.message);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+    mongoose.connection.on('connected', () => console.log('Connected to MongoDB')
+    );
+    await mongoose.connect(`${process.env.MONGODB_URI}/qucikblog`);
   } catch (error) {
-    console.error('❌ MongoDB connection FAILED:', error.message);
-    process.exit(1);
+    console.error('Error connecting to MongoDB:', error);
   }
 };
 
 export default connectDB;
-
