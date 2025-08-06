21e26aa38eba65516efa613ee1a9e2bd1616b831
aditya lucky <adityalucky52@gmail.com>
2025-08-06 22:49:15 +0530

    blog route update

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,21 +1,15 @@
 import express from "express";
-<<<<<<< HEAD
 import {addBlog, deleteBlog, getBlogId, getBlogs, togglePublish}  from "../controllers/blogController.js";
-=======
-import addBlog  from "../controllers/blogController.js";
->>>>>>> origin/main
 import upload from "../middleware/multer.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/add",upload.single("image"),auth,addBlog);
-<<<<<<< HEAD
 router.get("/all",getBlogs);
 router.get("/:blogId",getBlogId)
 router.get("/delete",auth, deleteBlog);
 router.post("/toggle-publish",auth, togglePublish);
-=======
->>>>>>> origin/main
 
-export default router;
+
+export default router; 
